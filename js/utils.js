d0c95ce3064d641b540c68058c6294628c63c306
Randy Bimm <bimmr6696@gmail.com>
2022-08-02 09:10:34 -0400

    Moved team members around

--- js/utils.js
+++ js/utils.js
@@ -1,24 +1,23 @@
 let sfAccounts = [
-  '5b6b0b38c7b3f042604e254c', // SiteForward
-  '5b44a4121ee2f32880ef9485', // Mandy
-  '5b71d892138837295d1d88d5', // Randy
-  '5ed534b953c1441f7930abfa', // Zain
-  '6053855b7d9f5556ecc9e33b' // Julien
-//   '5fd3d2192456636b40e14528', Staging Officer Account
+  '5b6b0b38c7b3f042604e254c',  // SiteForward
+  '5b44a4121ee2f32880ef9485',  // Mandy
+  '5b71d892138837295d1d88d5',  // Randy
+  '5ed534b953c1441f7930abfa',  // Zain
+//'5fd3d2192456636b40e14528',  // Staging Officer Account
 ];
 let mlsAccounts = [
   '5b7a252c87b3513edc0b86bb', // Debbie
   '5b7a254b87b3513edc0b86bc', // John
-  '5cab89722de8d2305492b333', // Kat
   '5d1391aa7c86f50a97009f18', // Rachel
   '5e82312ab7479c3245b2f88e', // Summer
   '5e82314cb7479c3245b2f891', // Suzanne
-  '5b6b0812c7b3f042604e253c' // MLS Sales Communication
+  '6053855b7d9f5556ecc9e33b', // Julien
+  '5b6b0812c7b3f042604e253c'  // MLS Sales Communication
 ];
 let msiAccounts = [
   '5b7a258c87b3513edc0b86be', // Janet
   '5b7a25ac9f5388026d43d977', // Sandy
-  '5b6b08c8fc61b959e4b69d79' // Market Conduct Compliance
+  '5b6b08c8fc61b959e4b69d79'  // Market Conduct Compliance
 ];
 let miscAccounts = [
   '5dd2f8e3d3547e4c268d5c42', // SFP - On Hold
@@ -27,8 +26,9 @@ let miscAccounts = [
   'all' // Under Construction
 ];
 let notActive = [
+  '5cab89722de8d2305492b333', // Kat
   '5b9490ea0420c067d6b37637', // Josee
-  '5f7f00c82820196e420db6bc' // Paul
+  '5f7f00c82820196e420db6bc'  // Paul
 ];
 
 let isSiteForward = function(id){
